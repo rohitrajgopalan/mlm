ee889cd237bce4d6c7d15430ac9522e693fc75a0
Rohit Gopalan <rohitgopalan@hotmail.com>
2020-07-26 15:37:29 +0930

    Update blue_spots_generator.py

--- blue_spots_generator.py
+++ blue_spots_generator.py
@@ -15,7 +15,7 @@ max_average_distance = 10000
 min_average_hierarchical_distance = 0
 max_average_hierarchical_distance = 10
 
-cols = ['Distance since Last Update', 'Error Penalty', 'Number of blue Nodes', 'Score for all Nodes', 'Average Distance', 'Distance Modifier', 'Average Hierarchical distance', 'H Distance Modifier', 'Score']
+cols = ['Distance since Last Update', 'Number of blue Nodes', 'Average Distance', 'Average Hierarchical distance', 'Score']
 
 if not isdir(join(dirname(realpath('__file__')), 'datasets')):
     mkdir(join(dirname(realpath('__file__')), 'datasets'))
@@ -25,17 +25,17 @@ if not isdir(join(dirname(realpath('__file__')), 'datasets', 'blue_spots')):
 
 for distance_since_last_update in range(min_distance_since_last_update, max_distance_since_last_update+1):
     error_penalty = distance_since_last_update * look_ahead_time_in_seconds * distance_error_base
-    new_data = {'Distance since Last Update': distance_since_last_update, 'Error Penalty': error_penalty}
+    new_data = {'Distance since Last Update': distance_since_last_update}
     for num_blue_nodes in range(min_blue_nodes, max_blue_nodes+1):
         score_for_all_nodes = num_blue_nodes * error_penalty
-        new_data.update({'Number of blue Nodes': num_blue_nodes, 'Score for all Nodes': score_for_all_nodes})
+        new_data.update({'Number of blue Nodes': num_blue_nodes})
         for average_distance in range(min_average_distance, max_average_distance+1):
             distance_modifier = math.pow(1-0.2, (average_distance/100)-1)
             df = pd.DataFrame(columns=cols)
-            new_data.update({'Average Distance': average_distance, 'Distance Modifier': distance_modifier})
+            new_data.update({'Average Distance': average_distance})
             for average_hierarchical_distance in range(min_average_hierarchical_distance, max_average_hierarchical_distance+1):
                 h_distance_modifier = math.pow(1-0.2, average_hierarchical_distance)
                 score = score_for_all_nodes * distance_modifier * h_distance_modifier
-                new_data.update({'Average Hierarchical distance': average_hierarchical_distance, 'H Distance Modifier': h_distance_modifier, 'Score': score})
+                new_data.update({'Average Hierarchical distance': average_hierarchical_distance, 'Score': score})
                 df = df.append(new_data, ignore_index=True)
             df.to_csv(join(dirname(realpath('__file__')), 'datasets', 'blue_spots', 'blue_spots_{0}_{1}.csv'.format((distance_since_last_update+num_blue_nodes+average_distance)+1, datetime.now().strftime("%Y%m%d%H%M%S"))), index=False)

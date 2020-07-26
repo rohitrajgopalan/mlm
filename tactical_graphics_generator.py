d2081019b715d90b8e97e4b5fb3a66146d128b6f
Rohit Gopalan <rohitgopalan@hotmail.com>
2020-07-26 15:40:04 +0930

    Update tactical_graphics_generator.py

--- tactical_graphics_generator.py
+++ tactical_graphics_generator.py
@@ -9,7 +9,7 @@ multiplier = 3
 start_cum_message_score = 49.925
 decay = 1/60
 
-cols = ['Age of Message', 'Cum Message cost', 'Score (Lazy)']
+cols = ['Age of Message', 'Score (Lazy)']
 df = pd.DataFrame(columns=cols)
 
 if not isdir(join(dirname(realpath('__file__')), 'datasets')):
@@ -24,6 +24,6 @@ while cum_message_cost >= 0:
     if cum_message_cost < 0:
         break
     score = cum_message_cost * multiplier
-    df = df.append({'Age of Message': age_of_message, 'Cum Message cost': cum_message_cost, 'Score (Lazy)': score}, ignore_index=True)
+    df = df.append({'Age of Message': age_of_message, 'Score (Lazy)': score}, ignore_index=True)
     age_of_message += 1
 df.to_csv(join(dirname(realpath('__file__')), 'datasets', 'tactical_graphics', 'tactical_graphics_{0}.csv'.format(datetime.now().strftime("%Y%m%d%H%M%S"))), index=False)

1d16f6bba94339622bc6ad86bb038b4553b02715
Rohit Gopalan <rohitgopalan1990@gmail.com>
2020-08-18 16:00:16 +0930

    Modify number of datasets for Red Spots

--- red_spots_generator.py
+++ red_spots_generator.py
@@ -68,6 +68,6 @@ def generate_data(data_type, max_num_files, max_rows):
                   index=False)
 
 
-generate_data('train', 40, 1000)
+generate_data('train', 80, 1000)
 if generate_test_data:
     generate_data('test', 1, 100)

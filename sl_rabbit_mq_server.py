7f4d75004f678e91eded48669a6b357a624e6e3c
Rohit Gopalan <rohitgopalan1990@gmail.com>
2020-11-09 15:25:44 +1030

    Modify python server based on the objects that will be sent from SMARTNet

--- sl_rabbit_mq_server.py
+++ sl_rabbit_mq_server.py
@@ -15,6 +15,8 @@ result_cols = ['regressor',
 
 
 class SLRabbitMQServer(RabbitMQServer):
+    message_types = ['text_messages', 'tactical_graphics', 'sos', 'blue_spots', 'red_spots']
+
     message_type_models = {
         'text_messages': {
             'model': None,
@@ -90,8 +92,7 @@ class SLRabbitMQServer(RabbitMQServer):
     }
 
     MODEL_CREATION = 'SET_MODEL'
-    COST = 'COST'
-    SCORE = 'SCORE'
+    COST = 'UPDATE_MODEL'
 
     def __init__(self):
         for message_type in self.message_type_models:
@@ -102,106 +103,88 @@ class SLRabbitMQServer(RabbitMQServer):
         super().__init__(queue_name='sl_request_queue')
 
     def _get_reply(self, request):
-        message_type = request['message_type']
-        request_type = request['request_type']
+        request_body = request['requestBody']
+        request_type = request['requestType']
 
         if request_type == self.COST:
+            message_type = request_body['message_type'].lower()
             feature_value_dict = {}
             if message_type == 'red_spots':
-                nearest_values = request['nearest_values']
+                nearest_values = request_body['nearest_values']
                 nearest_values = np.array(nearest_values)
                 nearest_values = np.sort(nearest_values)
                 nearest_values = nearest_values[:5]
-                request.update({'nearest_values': nearest_values})
+                request_body.update({'nearest_values': nearest_values})
                 for i in range(5):
                     feature_value_dict.update({'#{0} Nearest'.format(i + 1): nearest_values[i]})
-            label, actual = calculate_score(message_type, request)
+            label, actual = calculate_score(message_type, request_body)
 
             features = self.message_type_models[message_type]['features']
             cols_to_json = self.message_type_models[message_type]['cols_to_json']
 
             if self.message_type_models[message_type]['model'] is None:
-                return json.dumps({'message_type': message_type, label: actual, 'using_model': 0})
+                return -1
             else:
                 try:
                     for feature in features:
                         if feature in cols_to_json:
-                            feature_value_dict.update({feature: request[cols_to_json[feature]]})
-                    predicted = self.message_type_models[message_type]['model'].predict_then_fit(feature_value_dict)
-                    return json.dumps({'message_type': message_type, label: predicted, 'using_model': 1})
+                            feature_value_dict.update({feature: request_body[cols_to_json[feature]]})
+                    return self.message_type_models[message_type]['model'].predict_then_fit(feature_value_dict)
                 except:
-                    return json.dumps({'message_type': message_type, label: actual, 'using_model': 0})
-        elif request_type == self.SCORE:
-            if 'metric_type' in request:
-                metric_type = request['metric_type']
-                score = self.message_type_models[message_type]['model'].calculate_score(message_type, metric_type)
-                return json.dumps({'message_type': message_type, metric_type: score})
-            else:
-                mae = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mae')
-                mse = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mse')
-                return json.dumps({'message_type': message_type, 'mean_absolute_error': mae, 'mean_squared_error': mse})
+                    return actual
         elif request_type == self.MODEL_CREATION:
-            results = self.message_type_models[message_type]['results']
-            mse = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mse')
-            mae = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mae')
-            combinations = self.message_type_models[message_type]['combinations']
-            if self.message_type_models[message_type]['current_combination_index'] in range(0, len(combinations)):
-                combination = combinations[self.message_type_models[message_type]['current_combination_index']]
-                method_name, degree, interaction_only, include_bias, scaling_type, enable_normalization, use_grid_search = combination
-                if mse > -1 and mae > -1:
-                    results = results.append({'regressor': method_name,
-                                              'polynomial_degree': degree,
-                                              'polynomial_interaction_only': 'Yes' if interaction_only else 'No',
-                                              'polynomial_include_bias': 'Yes' if include_bias else 'No',
-                                              'scaling_type': scaling_type.name,
-                                              'enable_normalization': 'Yes' if enable_normalization else 'No',
-                                              'use_grid_search': 'Yes' if use_grid_search else 'No',
-                                              'mae': mae,
-                                              'mse': mse}, ignore_index=True)
-                    self.message_type_models[message_type].update({'results': results})
-            try:
-                if 'use_best' in request:
-                    use_best = request['use_best'] == 1
+            model_created = np.empty(5, dtype=np.bool)
+            for i, message_type in enumerate(self.message_types):
+                model_created[i] = self.create_model(message_type,
+                                                     request_body['use_best'] == 1,
+                                                     request_body['metric']) == 1
+            return 1 if model_created.all() else 0
+
+    def create_model(self, message_type, use_best, metric_type):
+        results = self.message_type_models[message_type]['results']
+        mse = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mse')
+        mae = self.message_type_models[message_type]['model'].calculate_score(message_type, 'mae')
+        combinations = self.message_type_models[message_type]['combinations']
+        if self.message_type_models[message_type]['current_combination_index'] in range(0, len(combinations)):
+            combination = combinations[self.message_type_models[message_type]['current_combination_index']]
+            method_name, degree, interaction_only, include_bias, scaling_type, enable_normalization, use_grid_search = combination
+            if mse > -1 and mae > -1:
+                results = results.append({'regressor': method_name,
+                                          'polynomial_degree': degree,
+                                          'polynomial_interaction_only': 'Yes' if interaction_only else 'No',
+                                          'polynomial_include_bias': 'Yes' if include_bias else 'No',
+                                          'scaling_type': scaling_type.name,
+                                          'enable_normalization': 'Yes' if enable_normalization else 'No',
+                                          'use_grid_search': 'Yes' if use_grid_search else 'No',
+                                          'mae': mae,
+                                          'mse': mse}, ignore_index=True)
+                self.message_type_models[message_type].update({'results': results})
+        try:
+            if use_best:
+                model = self.get_best_model(message_type, metric_type)
+                self.message_type_models[message_type].update({'model': model, 'actual': [], 'predicted': []})
+                return 1
+            else:
+                self.message_type_models[message_type]['current_combination_index'] += 1
+                if self.message_type_models[message_type]['current_combination_index'] in range(0, len(combinations)):
+                    combination = combinations[self.message_type_models[message_type]['current_combination_index']]
+                    model = ScikitModel(combination, message_type, self.message_type_models[message_type]['features'],
+                                        self.message_type_models[message_type]['label'])
+                    self.message_type_models[message_type].update({'model': model})
+                    return 1
                 else:
-                    use_best = False
-
-                metric_type = 'mse'
-
-                if 'use_metric' in request:
-                    metric_type = request['use_metric']
-
-                if use_best:
-                    model = self.get_best_model(message_type, metric_type)
-                    self.message_type_models[message_type].update({'model': model, 'actual': [], 'predicted': []})
-                    return json.dumps({'message_type': message_type, 'model_created': 1})
-                else:
-                    self.message_type_models[message_type]['current_combination_index'] += 1
-                    if self.message_type_models[message_type]['current_combination_index'] in range(0, len(combinations)):
-                        combination = combinations[self.message_type_models[message_type]['current_combination_index']]
-                        model = ScikitModel(combination, message_type, self.message_type_models[message_type]['features'], self.message_type_models[message_type]['label'])
-                        self.message_type_models[message_type].update({'model': model})
-                        return json.dumps({'message_type': message_type, 'model_created': 1})
-                    else:
-                        return json.dumps({'message_type': message_type, 'model_created': 0})
-            except:
-                return json.dumps({'message_type': message_type, 'model_created': 0})
+                    return 0
+        except:
+            return 0
 
     def get_best_model(self, message_type, metric_type):
         results = self.message_type_models[message_type]['results']
-        metric = ''
-        if metric_type in ['mae', 'mean_absolute_error']:
-            metric = 'mae'
-        elif metric_type in ['mse', 'mean_squared_error']:
-            metric = 'mse'
-        if len(metric) > 0:
-            lowest_val = np.min(results[metric])
-            for index, row in results.iterrows():
-                if row[metric] == lowest_val:
-                    combination = (row['regressor'], row['polynomial_degree'], row['polynomial_interaction_only'], row['polynomial_include_bias'],
-                                   row['scaling_type'], row['enable_normalization'], row['use_grid_search'])
-                    return ScikitModel(combination, message_type, self.message_type_models[message_type]['features'], self.message_type_models[message_type]['label'])
-        else:
-            return None
+        lowest_val = np.min(results[metric_type])
+        for index, row in results.iterrows():
+            if row[metric] == lowest_val:
+                combination = (row['regressor'], row['polynomial_degree'], row['polynomial_interaction_only'], row['polynomial_include_bias'],
+                               row['scaling_type'], row['enable_normalization'], row['use_grid_search'])
+                return ScikitModel(combination, message_type, self.message_type_models[message_type]['features'], self.message_type_models[message_type]['label'])
 
 
 # Before running the server, pull the RabbitMQ docker image:
